7b67cce3489c0353bcf228556fe76ea7b8967df9
Andrey Zakhvatoshin <mastetandron@gmail.com>
2025-09-03 21:02:50 +0300

    feat: добавлен sidebar

---
+++ frontend/src/components/Sidebar.vue
@@ -0,0 +1,33 @@
+<template>
+  <aside class="sidebar">
+    <nav>
+      <ul>
+        <li><router-link to="/">Проекты</router-link></li>
+        <li><router-link to="/about">О нас</router-link></li>
+        <li><router-link to="/contacts">Контакты</router-link></li>
+      </ul>
+    </nav>
+  </aside>
+</template>
+
+<style scoped>
+.sidebar {
+  width: 250px;
+  background: #FAFAFBFF;
+  color: white;
+  padding: 1rem;
+  box-sizing: border-box;
+  height: 100vh;
+  overflow-y: auto;
+}
+
+.sidebar a {
+  color: #565D6DFF;
+  text-decoration: none;
+  display: block;
+  margin-bottom: 0.5rem;
+}
+.sidebar a.router-link-exact-active {
+  font-weight: bold;
+}
+</style>
